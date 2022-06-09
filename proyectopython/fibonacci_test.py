4f43fd6edbf25a9fdbce90653a99b96c8c232fd7
ayolethangulo <anayoleth93@gmail.com>
2022-06-09 18:40:39 -0500

    C4 Hacer que la prueba pase -verde-

--- proyectopython/fibonacci_test.py
+++ proyectopython/fibonacci_test.py
@@ -18,5 +18,14 @@ def sumar_posicion(posicion):
             return 0
         elif posicion == 2:
             return 1
+        else:
+            a = 0
+            b = 1
+            c =0
+            for i in range(1, posicion):
+                a=b
+                b=c
+                c=a+b
+            return c
     else:
         return -1
