ecd168a8882b12509d2d6adeb4e6222d9ff9f987
ayolethangulo <anayoleth93@gmail.com>
2022-06-09 18:27:18 -0500

    C4 Escribir prueba -Rojo-

--- proyectopython/fibonacci_test.py
+++ proyectopython/fibonacci_test.py
@@ -9,6 +9,9 @@ def test_segunda_posicion():
 def test_validar_mayor_cero():
     assert sumar_posicion(-5)==-1
 
+def test_n_posicion():
+    assert sumar_posicion(7)==8
+
 def sumar_posicion(posicion):
     if posicion > 0 :
         if posicion == 1:
