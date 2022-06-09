83d775352e377e50bbe42fbcb2bdf732bd7677bf
ayolethangulo <anayoleth93@gmail.com>
2022-06-09 18:07:58 -0500

    C1 Refactorizacion

---
+++ proyectopython/fibonacci_test.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_primera_posicion():
+    assert sumar_posicion(1)==0
+
+def sumar_posicion(posicion):
+    return 0
--- test.py
+++
@@ -1,5 +0,0 @@
-def test_primera_posicion():
-    assert sumar_posicion(1)==0
-
-def sumar_posicion(posicion):
-    return 0
