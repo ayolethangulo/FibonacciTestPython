8c6055091f878cec3ebe3acb9796c8eb53619aca
ayolethangulo <anayoleth93@gmail.com>
2022-06-09 18:16:37 -0500

    C2 Hacer que la prueba pase -Verde-

--- proyectopython/fibonacci_test.py
+++ proyectopython/fibonacci_test.py
@@ -3,5 +3,11 @@ import pytest
 def test_primera_posicion():
     assert sumar_posicion(1)==0
 
+def test_segunda_posicion():
+    assert sumar_posicion(2)==1
+
 def sumar_posicion(posicion):
-    return 0
+    if posicion == 1:
+        return 0
+    elif posicion == 2:
+        return 1
