98153264c1dfa3c053755283d8ae584bd01a879b
ayolethangulo <anayoleth93@gmail.com>
2022-06-09 18:48:15 -0500

    Limpiando codigo, separando codigo

--- proyectopython/fibonacci_test.py
+++ proyectopython/fibonacci_test.py
@@ -1,4 +1,5 @@
 import pytest
+from main import sumar_posicion
 
 def test_primera_posicion():
     assert sumar_posicion(1)==0
@@ -10,22 +11,4 @@ def test_validar_mayor_cero():
     assert sumar_posicion(-5)==-1
 
 def test_n_posicion():
-    assert sumar_posicion(7)==8
-
-def sumar_posicion(posicion):
-    if posicion > 0 :
-        if posicion == 1:
-            return 0
-        elif posicion == 2:
-            return 1
-        else:
-            a = 0
-            b = 1
-            c =0
-            for i in range(1, posicion):
-                a=b
-                b=c
-                c=a+b
-            return c
-    else:
-        return -1
+    assert sumar_posicion(9)==21
--- proyectopython/main.py
+++ proyectopython/main.py
@@ -1,1 +1,17 @@
-print ("Hola")
+def sumar_posicion(posicion):
+    if posicion > 0 :
+        if posicion == 1:
+            return 0
+        elif posicion == 2:
+            return 1
+        else:
+            a = 0
+            b = 1
+            c =0
+            for i in range(1, posicion):
+                a=b
+                b=c
+                c=a+b
+            return c
+    else:
+        return -1
